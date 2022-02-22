26eca0d20a5d5d726ae8970a859a3e15fd1ec9b6
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2022-02-21 17:34:21 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     description='Query Ensembl for genes using free form search words (gget search) or fetch FTP download links by species (gget FetchTP).',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author_email='email@example.com',
     packages=find_packages(),
     install_requires=[
           'mysql-connector-python',
