66d265516943c3987dfa09a80fcb64ce9225c594
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2022-02-22 13:31:04 -0800

    Added beautifulsoup4 to install requires

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = read('README.md')
 
 setup(
     name='gget',
-    version='0.0.4',
+    version='0.0.5',
     license='MIT',
     author='Laura Luebbert',
     author_email='lauraluebbert@caltech.edu',
