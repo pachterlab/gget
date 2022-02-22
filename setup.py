0811823b9c4419d447b6c9b7abd85fd609c812b7
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2022-02-21 17:25:29 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+long_description = read('README.md')
+
 setup(
     name='gget',
     version='0.0.1',
     license='MIT',
     author='Laura Luebbert',
+    author_email='lauraluebbert@caltech.edu',
     description='Query Ensembl for genes using free form search words (gget search) or fetch FTP download links by species (gget FetchTP).',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author_email='email@example.com',
     packages=find_packages(),
     url='https://github.com/lauraluebbert/gget',
