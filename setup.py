ef341ab4323770d558e938cac6abab2b78ec01e9
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2022-02-25 00:55:52 -0800

    v0.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
     license='MIT',
     author='Laura Luebbert',
     author_email='lauraluebbert@caltech.edu',
-    description='Query Ensembl for genes using free form search words (gget search) or fetch FTP download links by species (gget FetchTP).',
+    maintainer='Laura Luebbert',
+    maintainer_email='lauraluebbert@caltech.edu',
+    description='Query Ensembl for genes using free form search words, look up genes/transcripts by Ensembl ID or fetch the latest FTPs by species.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
@@ -25,4 +27,16 @@ setup(
     entry_points={
         'console_scripts': ['gget=gget.gget:main'],
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Framework :: Jupyter',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Utilities',
 )
