a7af766e0fa0d4ebd37e416f922194912e0c195f
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2022-03-01 23:18:30 -0800

    Update gget.py

--- gget/gget.py
+++ gget/gget.py
@@ -3,8 +3,8 @@
 
 ## Import packages
 # Import gget main and utils
-from main import *
-from utils import *
+from gget.main import *
+from gget.utils import *
 
 # Packages for gget search
 import pandas as pd
