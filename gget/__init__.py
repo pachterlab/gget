4b54d279c4b9e46b64e7fb312f231ecc1ede2170
Laura Luebbert <laura.lbt60@gmail.com>
2022-04-05 13:44:12 -0700

    added imports to init

--- gget/__init__.py
+++ gget/__init__.py
@@ -1,3 +1,17 @@
+from .gget import (
+    info,
+    search,
+    ref,
+    seq,
+    blast,
+    muscle
+)
+
+from .main import (
+    main
+)
+
+
 __version__ = '0.0.18'
 __author__ = 'Laura Luebbert'
 __email__ = 'lauraluebbert@caltech.edu'
