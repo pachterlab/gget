262f7be2917573b4f5f25e70fcbb6f04c0e416be
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2022-04-05 13:54:53 -0700

    Update main.py

--- gget/main.py
+++ gget/main.py
@@ -11,7 +11,7 @@ from tabulate import tabulate
 # Custom functions
 from .__init__ import __version__
 from ._help import help_
-from . import (
+from .gget import (
     info,
     search,
     ref,
