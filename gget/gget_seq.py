62650a8f7f1abe2172533055936480edd2d3ae74
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2022-05-09 16:46:41 -0700

    Update gget_seq.py

--- gget/gget_seq.py
+++ gget/gget_seq.py
@@ -31,8 +31,8 @@ def seq(
                 Defines whether nucleotide or amino acid sequences are returned.
                 Nucleotide sequences are fetched from the Ensembl REST API server.
                 Amino acid sequences are fetched from the UniProt REST API server.
-    - isoforms  If True, returns the sequences of all known transcripts
-                (for gene IDs only) (default: False).
+    - isoforms  If True, returns the sequences of all known transcripts (default: False).
+                (Only for gene IDs in combination with seqtype='transcript'.)
     - save      If True: Save output FASTA to current directory.
 
     Returns a list (or FASTA file if 'save=True') containing the requested sequences.
