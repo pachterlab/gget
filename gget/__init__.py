9300a86d9dcb75e5e0ed07106aaba07559a93e26
lauraluebbert <laura.lbt60@gmail.com>
2022-08-06 10:14:23 -0700

    Updated BLAST tests result

--- gget/__init__.py
+++ gget/__init__.py
@@ -19,6 +19,6 @@ logging.basicConfig(
 # Mute numexpr threads info
 logging.getLogger("numexpr").setLevel(logging.WARNING)
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 __author__ = "Laura Luebbert"
 __email__ = "lauraluebbert@caltech.edu"
