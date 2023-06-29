2eba27f0c654fe7d7b83cd85b51169989277cf83
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2023-06-29 16:59:52 -0700

    Update gget_search.py

--- gget/gget_search.py
+++ gget/gget_search.py
@@ -45,7 +45,7 @@ def search(
                       (e.g.searchwords = ["GABA", "gamma-aminobutyric"]).
     - species         Species or database to be searched. Species should be passed in the format "genus_species", e.g. "homo_sapiens".
                       To pass a specific database, enter the name of the core database and release number, e.g. 'mus_musculus_dba2j_core_105_1'.
-                      All available databases for each Ensembl release can be found here: ttp://ftp.ensembl.org/pub/
+                      All available databases for each Ensembl release can be found here: http://ftp.ensembl.org/pub/
     - id_type         "gene" (default) or "transcript"
                       Defines whether genes or transcripts matching the searchwords are returned.
     - andor           "or" (default) or "and"
