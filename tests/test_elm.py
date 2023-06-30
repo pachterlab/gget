17b153082e2bce5d9bf0f7991805e77ae80eff68
choang <choang@caltech.edu>
2023-06-30 11:12:43 -0700

    Remove sorting and changed bad aa seq

--- tests/test_elm.py
+++ tests/test_elm.py
@@ -19,12 +19,11 @@ class Testelm(unittest.TestCase):
 
         time.sleep(sleep_time)
         result_to_test = elm(**elm_dict[test]["args"])
-        result_to_test = result_to_test.sort_values(by=['elm_identifier'], ascending=True)
 
         # replace \xa0 with a space.
         result_to_test.replace("\xa0", " ", regex=True, inplace=True)
         # cast all values to str add astype
-        result_to_test = result_to_test.astype(str).values.tolist().sort()
+        result_to_test = result_to_test.astype(str).values.tolist()
 
         self.assertListEqual(result_to_test, expected_result)
 
@@ -34,12 +33,12 @@ class Testelm(unittest.TestCase):
 
         time.sleep(sleep_time * 3)
         result_to_test = elm(**elm_dict[test]["args"])
-        result_to_test = result_to_test.sort_values(by=['elm_identifier'], ascending=True)
+
         # replace \xa0 with a space.
         result_to_test.replace("\xa0", " ", regex=True, inplace=True)
     
         # cast all values to str add astype
-        result_to_test = result_to_test.astype(str).values.tolist().sort()
+        result_to_test = result_to_test.astype(str).values.tolist()
 
         self.assertEqual(result_to_test, expected_result)
 
@@ -53,7 +52,7 @@ class Testelm(unittest.TestCase):
         # replace \xa0 with a space.
         result_to_test.replace("\xa0", " ", regex=True, inplace=True)
         # cast all values to str add astype
-        result_to_test = result_to_test.astype(str).values.tolist().sort()
+        result_to_test = result_to_test.astype(str).values.tolist()
 
         self.assertListEqual(result_to_test, expected_result)
 
