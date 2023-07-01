8f696c09fdc7e2586acc9b0e4544166d256f7e18
choang <choang@caltech.edu>
2023-06-30 17:12:52 -0700

    Change test 1 expected results

--- tests/test_elm.py
+++ tests/test_elm.py
@@ -33,7 +33,7 @@ class Testelm(unittest.TestCase):
         time.sleep(sleep_time)
         result_to_test = elm(**elm_dict[test]["args"])
         #replace \xa0 with a space.
-        result_to_test.replace("\xa0"," ", regex=True, inplace=True)
+        result_to_test.astype(str).replace("\xa0"," ", regex=True, inplace=True)
             # cast all values to str 
         result_to_test = result_to_test.astype(str).values.tolist()
         self.assertListEqual(result_to_test, expected_result)
@@ -53,6 +53,6 @@ class Testelm(unittest.TestCase):
 
     def test_elm_bad_aa_seq(self):
         test = "test5"
-        time.sleep(sleep_time)
+        time.sleep(sleep_time * 2)
         result_to_test = elm(**elm_dict[test]["args"])
         self.assertIsNone(result_to_test, "Bad AA sequence result is not None.")
