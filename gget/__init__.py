4ca4909ea7d108628bce814663bb405f7da0b071
choang <choang@caltech.edu>
2023-08-10 11:25:50 -0700

    remove elm from init

--- gget/__init__.py
+++ gget/__init__.py
@@ -12,7 +12,6 @@ from .gget_setup import setup
 from .gget_pdb import pdb
 from .gget_gpt import gpt
 from .gget_cellxgene import cellxgene
-from .gget_elm import elm
 
 import logging
 logging.basicConfig(
