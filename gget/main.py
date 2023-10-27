21835b5bde9c5ecde3070514b6d78e77ecf153a3
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2023-10-27 11:40:21 -0700

    Slight change to KEGG args

--- gget/main.py
+++ gget/main.py
@@ -785,6 +785,22 @@ def main():
         help="Add this flag if background genes are given as Ensembl gene IDs.",
     )
     parser_enrichr.add_argument(
+        "-ko",
+        "--kegg_out",
+        type=str,
+        default=None,
+        required=False,
+        help="Path to file to save the highlighted KEGG pathway image, e.g. path/to/folder/kegg_pathway.png.",
+    )
+    parser_enrichr.add_argument(
+        "-kr",
+        "--kegg_rank",
+        type=int,
+        default=1,
+        required=False,
+        help="Candidate pathway rank to be plotted in KEGG pathway image.",
+    )
+    parser_enrichr.add_argument(
         "-csv",
         "--csv",
         default=True,
@@ -827,22 +843,6 @@ def main():
         action="store_true",
         required=False,
         help="DEPRECATED - json is now the default output format (convert to csv using flag [--csv]).",
-    )
-    parser_enrichr.add_argument(
-        "-k",
-        "--kegg",
-        type=str,
-        required=False,
-        help=(
-            "Path to the png file the marked KEGG pathway images will be saved in, e.g. path/to/directory/pathway.png."
-        ),
-    )
-    parser_enrichr.add_argument(
-        "-r",
-        "--kegg_rank",
-        type=int,
-        required=False,
-        help=("Rank of the KEGG pathway to be plotted."),
     )
 
     ## gget archs4 subparser
@@ -2003,10 +2003,10 @@ def main():
             database=args.database,
             ensembl=args.ensembl,
             ensembl_bkg=args.ensembl_bkg,
+            kegg_out=args.kegg_out,
+            kegg_rank=args.kegg_rank,
             json=args.csv,
             verbose=args.quiet,
-            kegg=args.kegg,
-            kegg_rank=args.kegg_rank,
         )
 
         # Check if the function returned something
