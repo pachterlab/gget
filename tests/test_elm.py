6616e9de0489df323559aca3fb79054fac750fb8
lauraluebbert <laura.lbt60@gmail.com>
2023-11-05 19:39:30 -0800

    Add unittests for gget elm

--- tests/test_elm.py
+++ tests/test_elm.py
@@ -1,13 +1,9 @@
 import unittest
 
 # Library to test functions that have calls to print()
-from unittest import mock
-import os
-import contextlib
-import filecmp
 import json
 
-from gget.gget_elm import diamond, elm
+from gget.gget_elm import elm
 
 # Load dictionary containing arguments and expected results
 with open("./tests/fixtures/test_elm.json") as json_file:
@@ -15,41 +11,38 @@ with open("./tests/fixtures/test_elm.json") as json_file:
 
 
 class TestELM(unittest.TestCase):
-    def test_diamond(self):
-        # File with sequences to align
-        fasta = "tests/fixtures/input.fa"
-        # File the results will be saved in
-        out = "tests/fixtures/out.tsv"
-
-        # Run muscle (trying to use contextlib here to silence muscle return (unsuccesfully))
-        with contextlib.redirect_stdout(open(os.devnull, "w")):
-            diamond(fasta, out=out)
-
-        # Expected result
-        ref_path = "tests/fixtures/out.tsv"
-        self.assertTrue(
-            filecmp.cmp(out, ref_path, shallow=False),
-            "The reference and diamond blast are not the same.",
-        )
-    
-    def test_elm_uniprot_id(self):
+    def test_elm_uniprot_id_in_elm(self):
         test = "test1"
         expected_result = elm_dict[test]["expected_result"]
-        result_to_test = elm(**elm_dict[test]["args"])
-        # If result is a DataFrame, convert to list
-        if isinstance(result_to_test, pd.DataFrame):
-            result_to_test = result_to_test.values.tolist()
 
-        time.sleep(sleep_time)
+        restul1, result2 = elm(**elm_dict[test]["args"])
+        result_to_test = (
+            restul1.dropna(axis=1).values.tolist()
+            + result2.dropna(axis=1).values.tolist()[15:35]
+        )
+
         self.assertListEqual(result_to_test, expected_result)
-    
-    def test_elm_seq(self):
+
+    def test_elm_uniprot_id_new(self):
         test = "test2"
         expected_result = elm_dict[test]["expected_result"]
-        result_to_test = elm(**elm_dict[test]["args"])
-        # If result is a DataFrame, convert to list
-        if isinstance(result_to_test, pd.DataFrame):
-            result_to_test = result_to_test.values.tolist()
 
-        time.sleep(sleep_time)
+        restul1, result2 = elm(**elm_dict[test]["args"])
+        result_to_test = (
+            restul1.dropna(axis=1).values.tolist()
+            + result2.dropna(axis=1).values.tolist()[15:35]
+        )
+
+        self.assertListEqual(result_to_test, expected_result)
+
+    def test_elm_uniprot_aminoacidseq(self):
+        test = "test3"
+        expected_result = elm_dict[test]["expected_result"]
+
+        restul1, result2 = elm(**elm_dict[test]["args"])
+        result_to_test = (
+            restul1.dropna(axis=1).values.tolist()
+            + result2.dropna(axis=1).values.tolist()[15:35]
+        )
+
         self.assertListEqual(result_to_test, expected_result)
