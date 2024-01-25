8456f4795a132fcb0b804535e6f134760e2871f6
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2024-01-25 10:25:52 -0800

    Add sleep time to avoid 429 errors

--- tests/test_cosmic.py
+++ tests/test_cosmic.py
@@ -1,6 +1,7 @@
 import unittest
 import pandas as pd
 import json
+import time
 from gget.gget_cosmic import cosmic
 
 # Load
@@ -19,6 +20,8 @@ class TestCosmic(unittest.TestCase):
             result_to_test = result_to_test.values.tolist()
         self.assertListEqual(result_to_test, expected_result)
 
+    # Delay to avoid server overload errors
+    time.sleep(60)
     def test_cosmic_limit_and_pubmet(self):
         test = "test2"
         expected_result = cosmic_dict[test]["expected_result"]
@@ -28,6 +31,7 @@ class TestCosmic(unittest.TestCase):
             result_to_test = result_to_test.values.tolist()
         self.assertListEqual(result_to_test, expected_result)
 
+    time.sleep(60)
     def test_cosmic_json_and_genes(self):
         test = "test3"
         expected_result = cosmic_dict[test]["expected_result"]
@@ -37,6 +41,7 @@ class TestCosmic(unittest.TestCase):
             result_to_test = result_to_test.values.tolist()
         self.assertListEqual(result_to_test, expected_result)
 
+    time.sleep(60)
     def test_cosmic_samples(self):
         test = "test4"
         expected_result = cosmic_dict[test]["expected_result"]
@@ -46,6 +51,7 @@ class TestCosmic(unittest.TestCase):
             result_to_test = result_to_test.values.tolist()
         self.assertListEqual(result_to_test, expected_result)
 
+    time.sleep(60)
     def test_cosmic_studies(self):
         test = "test5"
         expected_result = cosmic_dict[test]["expected_result"]
@@ -55,6 +61,7 @@ class TestCosmic(unittest.TestCase):
             result_to_test = result_to_test.values.tolist()
         self.assertListEqual(result_to_test, expected_result)
 
+    time.sleep(60)
     def test_cosmic_cancer(self):
         test = "test6"
         expected_result = cosmic_dict[test]["expected_result"]
@@ -64,6 +71,7 @@ class TestCosmic(unittest.TestCase):
             result_to_test = result_to_test.values.tolist()
         self.assertListEqual(result_to_test, expected_result)
 
+    time.sleep(60)
     def test_cosmic_tumour(self):
         test = "test7"
         expected_result = cosmic_dict[test]["expected_result"]
