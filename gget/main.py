a1c3ec8bd054a72d65dea0a58ac3880757e8bda4
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-13 21:56:26 -0700

    improve codes to use dataverse API server

--- gget/main.py
+++ gget/main.py
@@ -36,6 +36,7 @@ from .gget_elm import elm
 from .gget_diamond import diamond
 from .gget_cosmic import cosmic
 from .gget_mutate import mutate
+from .gget_dataverse import dataverse
 from .gget_opentargets import opentargets, OPENTARGETS_RESOURCES
 from .gget_cbio import cbio_plot, cbio_search
 from .gget_bgee import bgee
@@ -2395,6 +2396,32 @@ def main():
         help="Does not print progress information.",
     )
 
+    ## dataverse parser arguments
+    dataverse_desc = "Download datasets from the Dataverse repositories."
+    parser_dataverse = parent_subparsers.add_parser(
+        "dataverse",
+        parents=[parent],
+        description=dataverse_desc,
+        help=dataverse_desc,
+        add_help=True,
+        formatter_class=CustomHelpFormatter,
+    )
+    parser_dataverse.add_argument(
+        "-o",
+        "--path",
+        type=str,
+        required=True,
+        help="Path to the directory the datasets will be saved in, e.g. 'path/to/directory'.",
+    )
+    parser_dataverse.add_argument(
+        "-t",
+        "--table",
+        type=str,
+        default=None,
+        required=False,
+        help="File containing the dataset IDs to download, e.g. 'datasets.tsv'.",
+    )
+
     ### Define return values
     args = parent_parser.parse_args()
 
@@ -2446,6 +2473,7 @@ def main():
         "opentargets": parser_opentargets,
         "cbio": parser_cbio,
         "bgee": parser_bgee,
+        "dataverse": parser_dataverse,
     }
 
     if len(sys.argv) == 2:
@@ -3367,3 +3395,18 @@ def main():
                 print(
                     bgee_results.to_json(orient="records", force_ascii=False, indent=4)
                 )
+
+    ## dataverse return
+    if args.command == "dataverse":
+        # Define separator based on file extension
+        if '.csv' in args.table:
+            sep = ','
+        elif '.tsv' in args.table:
+            sep = '\t'
+        # Run gget dataverse function
+        dataverse(
+            df = args.table,
+            path = args.out,
+            sep = sep,
+        )
+    
---
+++ tests/test_dataverse.py
@@ -0,0 +1,24 @@
+import unittest
+import pandas as pd
+from gget.gget_dataverse import dataverse
+import os
+import shutil
+
+class TestDataverse(unittest.TestCase):
+    def test_dataverse_download(self):
+        df = pd.DataFrame({
+            'id': [6180617],
+            'name': ['nodes'],
+            'type': ['tab']
+        })
+
+        dataverse(df, 'temp_datasets')
+
+        # Check if the file is downloaded
+        self.assertTrue(os.path.exists('temp_datasets/nodes.tab'))
+
+        # Clean up by removing the datasets folder
+        shutil.rmtree('temp_datasets')
+
+if __name__ == '__main__':
+    unittest.main()
