d43c5f256e43a83c5cf7375e904dd5ad12f176cb
techno-sam <77073745+techno-sam@users.noreply.github.com>
2024-07-05 15:59:58 -0700

    bgee: run 'black' formatter

--- gget/gget_bgee.py
+++ gget/gget_bgee.py
@@ -13,12 +13,15 @@ def _bgee_species(gene_id: str) -> int:
     :param gene_id: Ensembl gene ID
     :return: species ID
     """
-    response = requests.get("https://bgee.org/api/", params={
-        "display_type": "json",
-        "page": "gene",
-        "action": "general_info",
-        "gene_id": gene_id,
-    })
+    response = requests.get(
+        "https://bgee.org/api/",
+        params={
+            "display_type": "json",
+            "page": "gene",
+            "action": "general_info",
+            "gene_id": gene_id,
+        },
+    )
 
     if not response.ok:
         raise RuntimeError(
@@ -26,11 +29,11 @@ def _bgee_species(gene_id: str) -> int:
             "Please double-check the arguments and try again.\n"
         )
 
-    genes_data: list[dict[str, ...]] = response.json()['data']['genes']
+    genes_data: list[dict[str, ...]] = response.json()["data"]["genes"]
     assert len(genes_data) == 1
     gene_data: dict[str, ...] = genes_data[0]
 
-    species: int = gene_data['species']['genomeSpeciesId']
+    species: int = gene_data["species"]["genomeSpeciesId"]
     return species
 
 
@@ -45,13 +48,16 @@ def bgee_orthologs(gene_id: str) -> pd.DataFrame:
     species = _bgee_species(gene_id)
 
     # then obtain homologs
-    response = requests.get(f"https://bgee.org/api/", params={
-        "display_type": "json",
-        "page": "gene",
-        "action": "homologs",
-        "gene_id": gene_id,
-        "species_id": species,
-    })
+    response = requests.get(
+        f"https://bgee.org/api/",
+        params={
+            "display_type": "json",
+            "page": "gene",
+            "action": "homologs",
+            "gene_id": gene_id,
+            "species_id": species,
+        },
+    )
 
     if not response.ok:
         raise RuntimeError(
@@ -59,16 +65,19 @@ def bgee_orthologs(gene_id: str) -> pd.DataFrame:
             "Please double-check the arguments and try again.\n"
         )
 
-    homologs_data: list[dict[str, ...]] = response.json()['data']['orthologsByTaxon']
-    homologs_data: list[dict[str, ...]] = sum([v['genes'] for v in homologs_data], [])
+    homologs_data: list[dict[str, ...]] = response.json()["data"]["orthologsByTaxon"]
+    homologs_data: list[dict[str, ...]] = sum([v["genes"] for v in homologs_data], [])
 
-    return json_list_to_df(homologs_data, [
-        ("gene_id", "geneId"),
-        ("gene_name", "name"),
-        ("species_id", "species.id"),
-        ("genus", "species.genus"),
-        ("species", "species.speciesName"),
-    ])
+    return json_list_to_df(
+        homologs_data,
+        [
+            ("gene_id", "geneId"),
+            ("gene_name", "name"),
+            ("species_id", "species.id"),
+            ("genus", "species.genus"),
+            ("species", "species.speciesName"),
+        ],
+    )
 
 
 def bgee_expression(gene_id: str) -> pd.DataFrame:
@@ -79,15 +88,18 @@ def bgee_expression(gene_id: str) -> pd.DataFrame:
     """
     species = _bgee_species(gene_id)
 
-    response = requests.get("https://bgee.org/api/", params={
-        "display_type": "json",
-        "page": "gene",
-        "action": "expression",
-        "gene_id": gene_id,
-        "species_id": species,
-        "cond_param": ["anat_entity", "cell_type"],
-        "data_type": "all",
-    })
+    response = requests.get(
+        "https://bgee.org/api/",
+        params={
+            "display_type": "json",
+            "page": "gene",
+            "action": "expression",
+            "gene_id": gene_id,
+            "species_id": species,
+            "cond_param": ["anat_entity", "cell_type"],
+            "data_type": "all",
+        },
+    )
 
     if not response.ok:
         raise RuntimeError(
@@ -95,14 +107,17 @@ def bgee_expression(gene_id: str) -> pd.DataFrame:
             "Please double-check the arguments and try again.\n"
         )
 
-    expression_data: list[dict[str, ...]] = response.json()['data']['calls']
+    expression_data: list[dict[str, ...]] = response.json()["data"]["calls"]
 
-    df =  json_list_to_df(expression_data, [
-        ("anat_entity_id", "condition.anatEntity.id"),
-        ("anat_entity_name", "condition.anatEntity.name"),
-        ("score", "expressionScore.expressionScore"),
-        ("score_confidence", "expressionScore.expressionScoreConfidence"),
-        ("expression_state", "expressionState"),
-    ])
+    df = json_list_to_df(
+        expression_data,
+        [
+            ("anat_entity_id", "condition.anatEntity.id"),
+            ("anat_entity_name", "condition.anatEntity.name"),
+            ("score", "expressionScore.expressionScore"),
+            ("score_confidence", "expressionScore.expressionScoreConfidence"),
+            ("expression_state", "expressionState"),
+        ],
+    )
     df["score"] = df["score"].astype(float)
     return df
