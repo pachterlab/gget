1e146cdaf85f615014436fb524b279ebf6508baa
Laura Luebbert, Ph.D. <56094636+lauraluebbert@users.noreply.github.com>
2024-09-25 14:14:25 -0400

    background tests should be generated from json as well

--- tests/test_enrichr.py
+++ tests/test_enrichr.py
@@ -24,33 +24,33 @@ class TestEnrichr(unittest.TestCase, metaclass=from_json(enrichr_dict, enrichr))
 
         self.assertTrue(df.empty, "Invalid gene result is not empty data frame.")
 
-    def test_enrichr_background(self):
-        test = "test_enrichr_background"
-        expected_result = enrichr_dict[test]["expected_result"]
-        result_to_test = enrichr(**enrichr_dict[test]["args"])
-        # If result is a DataFrame, convert to list
-        if isinstance(result_to_test, pd.DataFrame):
-            result_to_test = result_to_test.values.tolist()[:20]
-            result_to_test = [
-                list(map(lambda x: x if x != math.inf else "inf", i))
-                for i in result_to_test
-            ]
+    # def test_enrichr_background(self):
+    #     test = "test_enrichr_background"
+    #     expected_result = enrichr_dict[test]["expected_result"]
+    #     result_to_test = enrichr(**enrichr_dict[test]["args"])
+    #     # If result is a DataFrame, convert to list
+    #     if isinstance(result_to_test, pd.DataFrame):
+    #         result_to_test = result_to_test.values.tolist()[:20]
+    #         result_to_test = [
+    #             list(map(lambda x: x if x != math.inf else "inf", i))
+    #             for i in result_to_test
+    #         ]
 
-        self.assertListEqual(result_to_test, expected_result)
+    #     self.assertListEqual(result_to_test, expected_result)
 
-    def test_enrichr_background_ensembl(self):
-        test = "test_enrichr_background_ensembl"
-        expected_result = enrichr_dict[test]["expected_result"]
-        result_to_test = enrichr(**enrichr_dict[test]["args"])
-        # If result is a DataFrame, convert to list
-        if isinstance(result_to_test, pd.DataFrame):
-            result_to_test = result_to_test.values.tolist()
-            result_to_test = [
-                list(map(lambda x: x if x != math.inf else "inf", i))
-                for i in result_to_test
-            ]
+    # def test_enrichr_background_ensembl(self):
+    #     test = "test_enrichr_background_ensembl"
+    #     expected_result = enrichr_dict[test]["expected_result"]
+    #     result_to_test = enrichr(**enrichr_dict[test]["args"])
+    #     # If result is a DataFrame, convert to list
+    #     if isinstance(result_to_test, pd.DataFrame):
+    #         result_to_test = result_to_test.values.tolist()
+    #         result_to_test = [
+    #             list(map(lambda x: x if x != math.inf else "inf", i))
+    #             for i in result_to_test
+    #         ]
 
-        self.assertListEqual(result_to_test, expected_result)
+    #     self.assertListEqual(result_to_test, expected_result)
 
     def test_enrichr_plot(self):
         # Number of plots before running enrichr plot
