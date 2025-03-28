ad1183e43b7672f736abde164e34e3c492a89f19
Laura Luebbert, Ph.D. <56094636+lauraluebbert@users.noreply.github.com>
2025-03-28 10:33:29 -0400

    Add sleep time for WB and FB IDs

--- tests/test_info.py
+++ tests/test_info.py
@@ -2,12 +2,15 @@ import unittest
 import unittest.mock
 import pandas as pd
 import json
+import time
 from gget.gget_info import info
 
 # Load dictionary containing arguments and expected results
 with open("./tests/fixtures/test_info.json") as json_file:
     info_dict = json.load(json_file)
 
+# Sleep time in seconds (wait [sleep_time] seconds between server requests to avoid 502 errors for WB and FB IDs)
+sleep_time = 10
 
 # todo convert to json loading once wormbase & flybase IDs are fixed. At that point, the json test framework will need a way to handle the ANY values
 class TestInfo(unittest.TestCase):
@@ -17,6 +20,7 @@ class TestInfo(unittest.TestCase):
         test = "test2"
         expected_result = info_dict[test]["expected_result"]
         result_to_test = info(**info_dict[test]["args"])
+        time.sleep(sleep_time)
         # If result is a DataFrame, convert to list
         if isinstance(result_to_test, pd.DataFrame):
             result_to_test = result_to_test.dropna(axis=1).values.tolist()
@@ -27,6 +31,7 @@ class TestInfo(unittest.TestCase):
         test = "test3"
         expected_result = info_dict[test]["expected_result"]
         result_to_test = info(**info_dict[test]["args"])
+        time.sleep(sleep_time)
         # If result is a DataFrame, convert to list
         if isinstance(result_to_test, pd.DataFrame):
             result_to_test = result_to_test.dropna(axis=1).values.tolist()
